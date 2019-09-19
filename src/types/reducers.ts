2460523bd2089ba98d6daaef0065d2134343b8aa
jfsalaza <jfsalaza@ucsd.edu>
2019-09-18 17:23:03 -0700

    Update reducers.ts

--- src/types/reducers.ts
+++ src/types/reducers.ts
@@ -28,7 +28,7 @@ import { Action, AnyAction } from './actions'
  */
 export type Reducer<S = any, A extends Action = AnyAction> = (
   state: S | undefined,
-  action: A
+  action: A | undefined
 ) => S
 
 /**
